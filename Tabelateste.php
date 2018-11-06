19ec1e8dd24649971dc6dfd12e7edab165d7ca93
Alex Torres <alextcarvalho98@gmail.com>
2018-11-06 11:12:27 -0400

    Agora vai

--- Tabelateste.php
+++ Tabelateste.php
@@ -9,10 +9,11 @@
  </head>
 <body>
 
-<table class="table">
+ <h1>Primeira tabela</h1>
+ <table class="table">
  <thead>
   <tr>
-   <th colspan="3" style="text-align: center;">KPI</th>
+   <th colspan="3" style="text-align: center; vertical-align: middle;">KPI</th>
    <th>Jan</th>
    <th>Feb</th>
    <th>Mar</th>
@@ -62,7 +63,7 @@
   </tr>
   <tr>
    <td>Improvement</td>
-   <td>Taxa <img src="uparrow.svg" alt="Logo" /></td>
+   <td>Taxa <img width="20px" src="upgreen.svg" alt="Logo" /></td>
   </tr>
   <tr>
    <td rowspan="3" style="text-align: center; vertical-align: middle">FCR</td>
@@ -97,7 +98,7 @@
   </tr>
   <tr>
    <td>Improvement</td>
-   <td>Rate</td>
+   <td>Rate <img width="20px" src="upgreen.svg" alt="Logo" /></td>
   </tr>
   <tr>
    <td rowspan="9" style="text-align: center; vertical-align: middle">Saengsang</td>
@@ -133,7 +134,7 @@
   </tr>
   <tr>
       <td>Improvement</td>
-      <td>Rate</td>
+      <td>Rate <img width="20px" src="upgreen.svg" alt="Logo" /></td>
   </tr>
   <td rowspan="3" style="text-align: center; vertical-align: middle">TLDR</td>
    <td>'17</td>
@@ -167,7 +168,7 @@
   </tr>
   <tr>
       <td>Improvement</td>
-      <td>Rate</td>
+      <td>Rate <img width="20px" src="upgreen.svg" alt="Logo" /></td>
   </tr>
   <td rowspan="3" style="text-align: center; vertical-align: middle">IFRR</td>
    <td>'17</td>
@@ -201,7 +202,259 @@
   </tr>
   <tr>
       <td>Improvement</td>
-      <td>Rate</td>
+      <td>Rate <img width="20px" src="upgreen.svg" alt="Logo" /></td>
+  </tr>
+  <tr>
+      <td rowspan="3" style="text-align: center; vertical-align: middle">Issue</td>
+      <td style="text-align: center";>Primeira linha</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td></td>
+      <td></td>
+  </tr>
+  <tr>
+      <td style="text-align: center";>Segunda linha</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td></td>
+      <td></td>
+  </tr>
+  <tr>
+      <td style="text-align: center";>Terceira linha</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td></td>
+      <td></td>
+  </tr>
+ </tbody>
+</table>
+ <h1>Segunda tabela (a do Edmilson)</h1>
+ <table class="table">
+ <thead>
+  <tr>
+   <th colspan="3"rowspan="2" style="text-align: center; vertical-align: middle;">KPI</th>
+   <th rowspan="2" style="text-align: center;">OCT'17 <br> Performance</th>
+   <th rowspan="2" style="text-align: center;">OCT'18 <br> Objective</th>
+   <th colspan="8" style="text-align: center;">OCT'18 Performance</th>
+   <th rowspan="2">Punctuation</th>
+   <th rowspan="2">Accomplishment</th>
+  </tr>
+  <tr>
+   <td>W40  </td>
+   <td>W41  </td>
+   <td>W42  </td>
+   <td>W43  </td>
+   <td>W44  </td>
+   <td>Accumulate </td>
+   <td colspan="2">Improvement rate</td>
+  </tr>
+ </thead>
+ <tbody>
+  <tr>
+   <td rowspan="6" style="text-align: center; vertical-align: middle">Market</td>
+   <td rowspan="3" style="text-align: center; vertical-align: middle">FFR</td>
+   <td>SVC Number</td>
+   <td>2,727</td>
+   <td> </td>
+   <td>2,191 </td>
+   <td>2,229 </td>
+   <td>2,280 </td>
+   <td>2,352 </td>
+   <td> </td>
+   <td>2,352 </td>
+   <td><img width="20px" src="circlegreen.svg" alt="Logo" /> </td>
+   <td style="color: green">x% <img width="20px" style="fill:green" src="downgreen.svg" alt="Logo" /> </td>
+   <td rowspan="3" style="text-align: center; vertical-align: bottom;">35 </td>
+   <td rowspan="3" style="text-align: center; vertical-align: bottom;">100% </td>
+  </tr>
+  <tr>
+   <td>Weighted cumulative spectral</td>
+   <td>138,474  </td>
+   <td>     </td>
+   <td>160,874  </td>
+   <td>160,888  </td>
+   <td>161,305  </td>
+   <td>161,409  </td>
+   <td></td>
+   <td>161,409</td>
+   <td><img width="20px" src="circlegreen.svg" alt="Logo" /> </td>
+   <td style="color: green"> 17% <img width="20px" style="fill:green" src="upgreen.svg" alt="Logo" /> </td>
+  </tr>
+  <tr>
+   <td>Market failure rate</td>
+   <td>1.97</td>
+   <td>1.90</td>
+   <td>1.36</td>
+   <td>1.39</td>
+   <td>1.41</td>
+   <td>1.46</td>
+   <td></td>
+   <td>1.46</td>
+   <td><img width="20px" src="circlegreen.svg" alt="Logo" /> </td>
+   <td style="color: green"> x% <img width="20px" style="fill:green" src="upgreen.svg" alt="Logo" /> </td>
+  </tr>
+  <tr>
+   <td rowspan="3" style="text-align: center; vertical-align: middle">FCR</td>
+   <td>'17</td>
+   <td>1,01</td>
+   <td>1,22</td>
+   <td>1,45</td>
+   <td>1,56</td>
+   <td>1,60</td>
+   <td>1,57</td>
+   <td>1,49</td>
+   <td>1,40</td>
+   <td>1,28</td>
+   <td>1,14</td>
+   <td>1,06</td>
+   <td>1,04</td>
+  </tr>
+  <tr>
+   <td>'18</td>
+   <td>0,76</td>
+   <td>0,87</td>
+   <td>0,95</td>
+   <td>1,21</td>
+   <td>1,33</td>
+   <td>1,24</td>
+   <td>1,18</td>
+   <td>1,13</td>
+   <td>1,09</td>
+   <td>1,05</td>
+   <td></td>
+   <td></td>
+  </tr>
+  <tr>
+   <td>Improvement</td>
+   <td>Rate <img width="20px" src="downred.svg" alt="Logo" /></td>
+  </tr>
+  <tr>
+   <td rowspan="9" style="text-align: center; vertical-align: middle">Saengsang</td>
+   <td rowspan="3" style="text-align: center; vertical-align: middle">PRR</td>
+   <td>'17</td>
+   <td>0</td>
+   <td>10</td>
+   <td>91</td>
+   <td>140</td>
+   <td>259</td>
+   <td>295</td>
+   <td>299</td>
+   <td>379</td>
+   <td>395</td>
+   <td>453</td>
+   <td>494</td>
+   <td>531</td>
+  </tr>
+  <tr>
+   <td>'18</td>
+   <td>728</td>
+   <td>712</td>
+   <td>636</td>
+   <td>579</td>
+   <td>567</td>
+   <td>564</td>
+   <td>501</td>
+   <td>486</td>
+   <td>479</td>
+   <td>482</td>
+   <td></td>
+   <td></td>
+  </tr>
+  <tr>
+      <td>Improvement</td>
+      <td>Rate <img width="20px" src="upgreen.svg" alt="Logo" /></td>
+  </tr>
+  <td rowspan="3" style="text-align: center; vertical-align: middle">TLDR</td>
+   <td>'17</td>
+   <td>3648</td>
+   <td>3597</td>
+   <td>3322</td>
+   <td>140</td>
+   <td>259</td>
+   <td>295</td>
+   <td>299</td>
+   <td>379</td>
+   <td>395</td>
+   <td>453</td>
+   <td>494</td>
+   <td>531</td>
+  </tr>
+  <tr>
+   <td>'18</td>
+   <td>2210</td>
+   <td>2051</td>
+   <td>2322</td>
+   <td>2277</td>
+   <td>567</td>
+   <td>564</td>
+   <td>501</td>
+   <td>486</td>
+   <td>479</td>
+   <td>482</td>
+   <td></td>
+   <td></td>
+  </tr>
+  <tr>
+      <td>Improvement</td>
+      <td>Rate <img width="20px" src="upgreen.svg" alt="Logo" /></td>
+  </tr>
+  <td rowspan="3" style="text-align: center; vertical-align: middle">IFRR</td>
+   <td>'17</td>
+   <td>0,56</td>
+   <td>0,0</td>
+   <td>0,31</td>
+   <td>0,24</td>
+   <td>2,87</td>
+   <td>0,34</td>
+   <td>2,72</td>
+   <td>0,22</td>
+   <td>0,14</td>
+   <td>0,85</td>
+   <td>0,00</td>
+   <td>0,22</td>
+  </tr>
+  <tr>
+   <td>'18</td>
+   <td>0,56</td>
+   <td>0,0</td>
+   <td>0,31</td>
+   <td>0,24</td>
+   <td>2,87</td>
+   <td>0,34</td>
+   <td>2,72</td>
+   <td>0,22</td>
+   <td>0,14</td>
+   <td>0,85</td>
+   <td></td>
+   <td></td>
+  </tr>
+  <tr>
+      <td>Improvement</td>
+      <td>Rate <img width="20px" src="upgreen.svg" alt="Logo" /></td>
   </tr>
   <tr>
       <td rowspan="3" style="text-align: center; vertical-align: middle">Issue</td>
